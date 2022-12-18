328585d8d25ac6f9fc47c612638340db44530772
Nazik <nazikalm@gmail.com>
2022-12-17 20:02:05 -0500

    Refactor Client function into a class

--- src/meraki/client.ts
+++ src/meraki/client.ts
@@ -3,7 +3,7 @@ import { DEFAULT_MERAKI_TIMEOUT } from '../settings.js'
 import { IClientParams } from '../types/global'
 
 export interface IClientOperation {
-    [method: string]: () => Promise<AxiosResponse<any, any>>
+    // [method: string]: () => Promise<AxiosResponse<any, any>>
 }
 
 export interface IClientProperties {
@@ -15,18 +15,25 @@ export interface IClientProperties {
 
 export type IClient = IClientProperties & IClientOperation
 
-export function Client(this: IClient, config: IClientParams) {
-    this.api_key = config.api_key
-    this.host = config.base_url || 'https://api.meraki.com'
-    this.base_path = config.base_path || '/api/v1'
-    this.client = axios.create({
-        baseURL: this.host + this.base_path,
-        timeout: config.timeout || DEFAULT_MERAKI_TIMEOUT,
-        headers: {
-            'User-Agent': config.user_agent || 'JavascriptSDK BoundlessDigital',
-            'X-Cisco-Meraki-API-Key': this.api_key
-        }
-    })
+export class Client implements IClient {
+    api_key: string;
+    host: string;
+    base_path: string;
+    client: Axios;
+
+    constructor(config: IClientParams) {
+        this.api_key = config.api_key
+        this.host = config.base_url || 'https://api.meraki.com'
+        this.base_path = config.base_path || '/api/v1'
+        this.client = axios.create({
+            baseURL: this.host + this.base_path,
+            timeout: config.timeout || DEFAULT_MERAKI_TIMEOUT,
+            headers: {
+                'User-Agent': config.user_agent || 'JavascriptSDK BoundlessDigital',
+                'X-Cisco-Meraki-API-Key': this.api_key
+            }
+        })
+    }
 }
 
 interface IOperation {
